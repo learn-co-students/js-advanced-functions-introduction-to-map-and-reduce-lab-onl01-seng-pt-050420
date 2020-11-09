a6b20d1439f739409c463734300929fd03f6cdbb
C0mbed <matt@farncombe.co>
2020-11-09 14:01:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(num => num * -1);
+};
+
+function mapToNoChange(arr) {
+    return arr.map(num => num);
+};
+
+function mapToDouble(arr) {
+    return arr.map(num => num * 2);
+};
+
+function mapToSquare(arr) {
+    return arr.map(num => num * num);
+};
+
+function reduceToTotal(arr, start=0) {
+    return arr.reduce(function(total, current) {
+        return total + current;
+    }, start);
+};
+
+function reduceToAllTrue(arr) {
+    return arr.reduce(function(result, value) {
+        return result && !!value;
+    }, true);
+};
+
+function reduceToAnyTrue(arr) {
+    return arr.reduce(function(result, value) {
+        return result || !!value;
+    }, false);
+};
